c8075fe8d776d818085252a5e948fed3dedd755e
resume12 <52920283+PaulosKapa@users.noreply.github.com>
2025-05-24 10:51:10 +0300

    2

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ def words(specified):
     with open("test.txt", 'r', encoding="utf-8") as file:
         for line in file:
             # Split the line into words (split() handles multiple spaces)
-            words = line.split()
-            for word in words:
-                if word == specified:
-                    lines_words.append(words)
+            words_in_line  = line.split()
+            if specified in words_in_line and "Π:" in words_in_line:
+                # Append the entire line's words as a sublist
+                lines_words.append(words_in_line)
     return(lines_words)
 
-print(words("πινακίδα"))
+
+def choose_label():
+    for word_array  in words("πινακίδα"):
+        for word in word_array:
+            print(word)
+
+choose_label()
