73db5ac6ed2c60fe8f6366e4a96d54d4b83dfb57
resume12 <52920283+PaulosKapa@users.noreply.github.com>
2025-05-24 10:26:12 +0300

    start

---
+++ main.py
@@ -0,0 +1,12 @@
+def words(specified):
+    lines_words = []
+    with open("test.txt", 'r', encoding="utf-8") as file:
+        for line in file:
+            # Split the line into words (split() handles multiple spaces)
+            words = line.split()
+            for word in words:
+                if word == specified:
+                    return word
+
+
+print(words("πινακίδα"))
